225fdf28453d73ca80149ce9c8b2c87bb6791a66
Sigvart M. Hovland <sigvart.m@gmail.com>
2013-09-03 18:12:46 +0200

    Added the latest version of the file with working uart and wired latch and tested latch

--- node1/main.c
+++ node1/main.c
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include <util/delay.h>
 #include "uart.h"
 #include "setup.h"
 
@@ -7,11 +8,34 @@
 int main(void)
 {
 	uart_init(MYUBRR);
-	DDRB  |= (1<<DDB0);
 	
+	//PORTE |= (1<<PE1);
+	//DDRB  |= (1<<DDB0);
+	
+	DDRA  |= (1<<PA0);
+	DDRE  |= (1<<PE1);
+	
+	set_bit(PORTE,PE1);
+	_delay_ms(1000);
+	set_bit(PORTA,PA0);
+	_delay_ms(1000);
+	
+	clear_bit(PORTA,PA0);
+	_delay_ms(1000);
+	clear_bit(PORTE,PE1);
+	
+	char c;
+
 	while(1)
     {
-		uart_putchar('U',uart);
-		//fprintf(uart, "Hello, World!\n"); 
+		
+	
+		//uart_putchar('U',uart);
+		//fprintf(uart,"Hello, World!\n");
+		//printf("Hello, World!\n\r");
+		c = uart_recchar();
+		printf("%c\n\r",c);
     }
+	
+	
 }
